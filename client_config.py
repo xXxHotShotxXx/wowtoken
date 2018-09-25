b22cfb10da75cccd9a9ba1282f6cd0d3efa0f846
xXxHotShotxXx <hotshot.twitch@gmail.com>
2018-09-24 21:16:54 -0400

    change

--- client_config.py
+++ client_config.py
@@ -5,4 +5,4 @@ class ClientConfig(object):
     COMPANY_NAME = 'Hot-Gamez'
     UPDATE_URLS = ['https://github.com/xXxHotShotxXx/wowtoken']
     MAX_DOWNLOAD_RETRIES = 3
-    PYIU_AWS_ID = "7063ad4d0859a0877aa3f5fb068309406ca2ae7668150d5405a52e0eedb40cee"
+    PYIU_AWS_ID = '7063ad4d0859a0877aa3f5fb068309406ca2ae7668150d5405a52e0eedb40cee'
